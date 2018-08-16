467ca3786b31b96c6972ed842a9f805354656175
Janos Follath <janos.follath@arm.com>
2018-08-16 11:19:49 +0100

    ECDH: Add setup to the context lifecycle

--- library/ecdh.c
+++ library/ecdh.c
@@ -38,16 +38,12 @@
 
 #include <string.h>
 
-#if defined(MBEDTLS_ECDH_LEGACY_CONTEXT)
-typedef mbedtls_ecdh_context mbedtls_ecdh_context_mbed;
+#if defined(MBEDTLS_PLATFORM_C)
+#include "mbedtls/platform.h"
+#else
+#define mbedtls_calloc calloc
+#define mbedtls_free   free
 #endif
-
-int mbedtls_ecdh_can_do( mbedtls_ecp_group_id gid )
-{
-    /* At this time, all groups support ECDH. */
-    (void) gid;
-    return 1;
-}
 
 #if !defined(MBEDTLS_ECDH_GEN_PUBLIC_ALT)
 /*
@@ -185,6 +181,47 @@ static int ecdh_setup_internal( mbedtls_ecdh_context_mbed *ctx,
 
 /*
  * Setup context
+ */
+int mbedtls_ecdh_setup( mbedtls_ecdh_context *ctx, mbedtls_ecp_group_id grp )
+{
+    int ret;
+#if defined(MBEDTLS_ECDH_LEGACY_CONTEXT)
+    mbedtls_ecdh_context *real_ctx;
+
+    if( ctx == NULL )
+        return( MBEDTLS_ERR_ECP_BAD_INPUT_DATA );
+
+    real_ctx = ctx;
+#else
+    mbedtls_ecdh_context_mbed *real_ctx;
+
+    if( ctx == NULL || ctx->ctx.mbed_ecdh != NULL )
+        return( MBEDTLS_ERR_ECP_BAD_INPUT_DATA );
+
+    ctx->ctx.mbed_ecdh = (mbedtls_ecdh_context_mbed*) mbedtls_calloc ( 1,
+                            sizeof( mbedtls_ecdh_context_mbed ) );
+
+    if( ctx->ctx.mbed_ecdh == NULL )
+        return( MBEDTLS_ERR_ECDH_ALLOC_FAILED );
+
+    ctx->var = MBEDTLS_ECDH_VARIANT_MBED;
+    ctx->grp = grp;
+
+    real_ctx = ctx->ctx.mbed_ecdh;
+#endif
+
+    ret = mbedtls_ecp_group_load( &real_ctx->grp, grp );
+    if( ret != 0 )
+    {
+        mbedtls_ecdh_free( ctx );
+        return( MBEDTLS_ERR_ECP_FEATURE_UNAVAILABLE );
+    }
+
+    return( 0 );
+}
+
+/*
+ * Free context
  */
 int mbedtls_ecdh_setup( mbedtls_ecdh_context *ctx, mbedtls_ecp_group_id grp_id )
 {
@@ -403,6 +440,7 @@ int mbedtls_ecdh_read_params( mbedtls_ecdh_context *ctx,
                               const unsigned char *end )
 {
     int ret;
+    mbedtls_ecp_group_id grp_id;
 #if defined(MBEDTLS_ECDH_LEGACY_CONTEXT)
     mbedtls_ecdh_context *real_ctx;
 
@@ -413,15 +451,22 @@ int mbedtls_ecdh_read_params( mbedtls_ecdh_context *ctx,
 #else
     mbedtls_ecdh_context_mbed *real_ctx;
 
-    if( ctx == NULL || ctx->ctx.mbed_ecdh == NULL )
+    if( ctx == NULL || ctx->ctx.mbed_ecdh != NULL )
         return( MBEDTLS_ERR_ECP_BAD_INPUT_DATA );
 
     real_ctx = ctx->ctx.mbed_ecdh;
 #endif
 
-    if( ( ret = mbedtls_ecp_tls_read_group( &real_ctx->grp, buf, end - *buf ) )
+    if( ( ret = mbedtls_ecp_tls_read_group_id( &grp_id, buf, end - *buf ) )
             != 0 )
         return( ret );
+
+    if( ( ret = mbedtls_ecdh_setup( ctx, grp_id ) ) != 0 )
+        return( ret );
+
+#if !defined(MBEDTLS_ECDH_LEGACY_CONTEXT)
+    real_ctx = ctx->ctx.mbed_ecdh;
+#endif
 
     if( ( ret = mbedtls_ecp_tls_read_point( &real_ctx->grp, &real_ctx->Qp, buf,
                                             end - *buf ) ) != 0 )
@@ -452,23 +497,23 @@ static int ecdh_get_params_internal( mbedtls_ecdh_context_mbed *ctx,
 {
     int ret;
 #if defined(MBEDTLS_ECDH_LEGACY_CONTEXT)
-    mbedtls_ecdh_context *real_ctx;
+    mbedtls_ecdh_context *real_ctx = ctx;
 
     if( ctx == NULL )
         return( MBEDTLS_ERR_ECP_BAD_INPUT_DATA );
-
-    real_ctx = ctx;
 #else
     mbedtls_ecdh_context_mbed *real_ctx;
 
-    if( ctx == NULL || ctx->ctx.mbed_ecdh == NULL )
+    if( ctx == NULL || ctx->ctx.mbed_ecdh != NULL )
         return( MBEDTLS_ERR_ECP_BAD_INPUT_DATA );
-
-    real_ctx = ctx->ctx.mbed_ecdh;
 #endif
 
-    if( ( ret = mbedtls_ecp_group_copy( &real_ctx->grp, &key->grp ) ) != 0 )
+    if( ( ret = mbedtls_ecdh_setup( ctx, key->grp.id ) ) != 0 )
         return( ret );
+
+#if !defined(MBEDTLS_ECDH_LEGACY_CONTEXT)
+    real_ctx = ctx->ctx.mbed_ecdh;
+#endif
 
     /* If it's not our key, just import the public part as Qp */
     if( side == MBEDTLS_ECDH_THEIRS )
