93141aeb5540f91d4444b92eb51c49ecafbd24e6
Christoph M. Wintersteiger <cwinter@microsoft.com>
2018-09-27 20:12:45 +0100

    Added EdDSA certificates (some bugs remain).
    Fixed MBEDTLS_EDDSA_C macro checks, fix for EdDSA private keys.
    Added experimental EdDSA ciphersuite.

--- library/pk_wrap.c
+++ library/pk_wrap.c
@@ -722,6 +722,12 @@ const mbedtls_pk_info_t mbedtls_rsa_alt_info = {
 
 
 #if defined(MBEDTLS_EDDSA_C)
+static size_t eddsa_get_bitlen( const void *ctx )
+{
+    ( void )ctx;
+    return( 256 );
+}
+
 static int eddsa_can_do( mbedtls_pk_type_t type )
 {
     return( type == MBEDTLS_PK_EDDSA );
@@ -754,6 +760,14 @@ static int eddsa_sign_wrap( void *ctx, mbedtls_md_type_t md_alg,
             hash, hash_len, sig, MBEDTLS_EDDSA_MAX_LEN, sig_len, f_rng, p_rng ) );
 }
 
+static int eddsa_check_pair( const void *pub, const void *prv )
+{
+    ( void )pub;
+    ( void )prv;
+    printf("EdDSA check pair not implemented yet.");
+    return( MBEDTLS_ERR_ECP_BAD_INPUT_DATA );
+}
+
 static void *eddsa_alloc_wrap( void )
 {
     void *ctx = mbedtls_calloc( 1, sizeof( mbedtls_eddsa_context ) );
@@ -770,19 +784,26 @@ static void eddsa_free_wrap( void *ctx )
     mbedtls_free( ctx );
 }
 
+static void eddsa_debug( const void *ctx, mbedtls_pk_debug_item *item )
+{
+    item->type = MBEDTLS_PK_DEBUG_EDDSA;
+    item->name = "EdDSA keys: ";
+    item->value = &( ( mbedtls_eddsa_context* )ctx )->keys;
+}
+
 const mbedtls_pk_info_t mbedtls_eddsa_info = {
     MBEDTLS_PK_EDDSA,
     "EdDSA",
-    eckey_get_bitlen,     /* Compatible key structures */
+    eddsa_get_bitlen,     /* Compatible key structures */
     eddsa_can_do,
     eddsa_verify_wrap,
     eddsa_sign_wrap,
     NULL,
     NULL,
-    eckey_check_pair,   /* Compatible key structures */
+    eddsa_check_pair,     /* Compatible key structures */
     eddsa_alloc_wrap,
     eddsa_free_wrap,
-    eckey_debug,        /* Compatible key structures */
+    eddsa_debug,          /* Compatible key structures */
 };
 #endif /* MBEDTLS_EDDSA_C */
 
